29c4b4905e0afd48753a478691d7acc86aac78a0
Juan Eduardo Flores <juanedflores@gmail.com>
2020-07-09 08:53:48 -0500

    made grid.js hydra functions easier to read

--- grid.js
+++ grid.js
@@ -1,4 +1,6 @@
 let toggle = false;
+// grid.js creates a grid that changes direction after passing fft threshold.
+// Author: Juan Eduardo Flores
 let xdir = 1;
 let tsnap = 0;
 let psnap = 0;
@@ -37,4 +39,10 @@ changeDirection = (bin, time, speed) => {
   }
   return value;
 };
-shape(2, 0.01, 0.001).scrollY(0.25).add(shape(2, 0.01, 0.001).scrollY(-0.25)).scrollY(() => changeDirection(0, time, 0.25)).add(shape(2, 0.01, 0.001).rotate(Math.PI / 2).scrollX(0.25).add(shape(2, 0.01, 0.001).rotate(Math.PI / 2).scrollX(-0.25)).scrollX(() => changeDirection(0, time, 0.25))).repeat(3, 3).out(o0);
+shape(2, 0.01, 0.001).scrollY(0.25)
+  .add(shape(2, 0.01, 0.001).scrollY(-0.25))
+  .scrollY(() => changeDirection(0, time, 0.25))
+  .add(shape(2, 0.01, 0.001).rotate(Math.PI / 2).scrollX(0.25)
+    .add(shape(2, 0.01, 0.001).rotate(Math.PI / 2).scrollX(-0.25))
+    .scrollX(() => changeDirection(0, time, 0.25)))
+  .repeat(3, 3).out(o0);
